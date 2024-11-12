472a31b8f03e02772a48f7ad4bb46a6260606680
Ryan Ramage <r@mage.rocks>
2024-11-12 07:58:12 -0700

    handle shutdown

--- example/basic.mjs
+++ example/basic.mjs
@@ -6,7 +6,19 @@ console.log('invite', invite)
 async function run () {
   const room = new BreakoutRoom({ invite })
   await room.ready()
-  setTimeout(() => room.exit(), 25000)
+
+  process.stdin.on('data', (data) => {
+    room.message(data.toString())
+  })
+
+  const shutdown = () => {
+    console.log('shutting down')
+    room.exit()
+    process.exit(0)
+  } 
+  process.on('SIGINT', shutdown)
+  process.on('SIGTERM', shutdown)
 }
+
 
 run()
